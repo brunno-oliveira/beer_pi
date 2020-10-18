f718c53c7c00efac429b0cec57e2b16e9d40bd53
Brunno Oliveira <brunnokick@gmail.com>
2020-10-18 20:52:53 -0300

    carga no google sheets para um sensor

--- sheet.py
+++ sheet.py
@@ -3,6 +3,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
+
 class GSheet:
     def __init__(self):
         self.scope = [
@@ -11,20 +12,22 @@ class GSheet:
             "https://www.googleapis.com/auth/drive.file",
             "https://www.googleapis.com/auth/drive",
         ]
-        self.cred = "creds.json"
+        self.cred = "/home/pi/git/beer_pi/creds.json"
         self.client = None
         self.connect()
 
     def connect(self):
         print('Connection do GSheets..')
-        creds = ServiceAccountCredentials.from_json_keyfile_name(self.cred, self.scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_name(
+            self.cred, self.scope)
         self.client = gspread.authorize(creds)
 
     def update(
-        self, 
-        temp: float, 
-        sheet_key: str = "1x0qI0HRQstLbYiUE4fnWGu1oguK0bK9Pp3lR7UwqlWQ"):
-        sheet =  self.client.open_by_key(sheet_key).sheet1  # Open the spreadhseet
+            self,
+            temp: float,
+            sheet_key: str = "1x0qI0HRQstLbYiUE4fnWGu1oguK0bK9Pp3lR7UwqlWQ"):
+        sheet = self.client.open_by_key(
+            sheet_key).sheet1  # Open the spreadhseet
 
         columns = {
             'A': 'TIMESTAMP',
@@ -34,26 +37,26 @@ class GSheet:
             'E': 'TEMPERATURA',
             'F': 'INFO'}
 
-        sensores = ['INTERNO', 'EXTERNO','TESTE']
+        sensores = ['INTERNO', 'EXTERNO', 'TESTE']
 
         now = datetime.now()
 
         print('Updating row..')
 
-        values =  [
-                    datetime.timestamp(now),
-                    str(now.date()), 
-                    str(now.time()), 
-                    'TESTE',
-                    temp,
-                    'sensor na mesa'
-                ]
+        values = [
+            datetime.timestamp(now),
+            str(now.date()),
+            str(now.time()),
+            'TESTE',
+            temp,
+            'SENSOR NO ISOPOR'
+        ]
         print('VALUES:')
         print(values)
         sheet.append_row(values)
 
         print('done')
 
+
 if __name__ == "__main__":
     GSheet().update(temp=25.5)
-    
