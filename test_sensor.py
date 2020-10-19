d0ad3841f0cce8842b7ce51fc4ed5e63ce0d5287
Brunno Oliveira <brunnokick@gmail.com>
2020-10-18 23:07:40 -0300

    multiple senso

--- test_sensor.py
+++ test_sensor.py
@@ -1,7 +1,13 @@
 import time
 from w1thermsensor import W1ThermSensor
 
+# Test single sensor
 sensor = W1ThermSensor()
 temperature_in_celsius = sensor.get_temperature()
+print('Testing single sensor: {}'.format(temperature_in_celsius))
+print()
 
-print(temperature_in_celsius)
+# Multiple sensors
+for sensor in W1ThermSensor.get_available_sensors():
+    print("Sensor %s has temperature %.2f" %
+          (sensor.id, sensor.get_temperature()))
